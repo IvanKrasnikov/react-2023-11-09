5a89f420bf03b9a7c1f17e6f0cadc40dbbfe669f
123456 <IKrasnikov@inno.tech>
2023-11-23 01:52:13 +0300

    feat: filter restaurant by category/name

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Category } from '../category/Category';
 import './categories.styles.css';
 
+export const Categories = ({ categories, onSelectCategory }) => {
 
-export const Categories = ({ categories }) => {
     return (
         <div className='categories'>
             {/** TODO: ask: what we should use as key when there are no data like id's ? */}
-            {categories.map((category, i) => <Category key={i} title={category} />)}
+            {categories.map((category, i) => <Category key={i} title={category} onClick={() => onSelectCategory(category)} />)}
         </div>
     );
 };
--- src/pages/restaurants-page/RestaurantsPage.jsx
+++ src/pages/restaurants-page/RestaurantsPage.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Categories } from '../../components/categories/Categories';
 import { Restaurants } from '../../components/restaurants/Restaurants';
 
 export const RestaurantsPage = ({ restaurants }) => {
+    const [selectedCategory, setSelectedCategory] = useState(null);
     const names = restaurants.map((restaurant) => restaurant.name);
+
+    const selectedRestaurants = restaurants.filter((restaurant) => {
+        return restaurant.name === selectedCategory;
+    });
+
+    if (!restaurants.length) {
+        return null;
+    }
 
     return (
         <>
-            {names?.length > 0 && <Categories categories={names} />}
-            {restaurants && <Restaurants restaurants={restaurants} />}
+            {names?.length > 0 && <Categories categories={names} onSelectCategory={setSelectedCategory} />}
+            {selectedCategory && <Restaurants restaurants={selectedRestaurants} />}
         </>
     );
 };
